9c806fdf623d2e92ddb710312411a92856dea3b1
Lazar971 <milosavljevic616@fon.bg.ac.rs>
2018-03-11 21:28:04 +0100

    Dodata klasa Menjacnica koja implementira interfejs IMenacnica

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,29 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import interfejs.IMenjacnica;
+import valuta.Kurs;
+import valuta.Valuta;
+
+public class Menjacnica implements IMenjacnica {
+
+	@Override
+	public void dodajKurs(Valuta v, Kurs k) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs(Valuta v, GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pronadjiKurs(Valuta v, GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
